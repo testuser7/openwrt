2680215b1eba8c716d293826ff8254a3d7e93e61
Paweł Owoc <frut3k7@gmail.com>
2024-12-12 19:51:27 +0100

    qualcommax: ipq807x: Update u-boot env only when changes for MX4200/MX5300/MX8500
    Update u-boot env variables only when changes are made.
    
    Signed-off-by: Paweł Owoc <frut3k7@gmail.com>

--- target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq807x/base-files/lib/upgrade/platform.sh
@@ -179,15 +179,30 @@ platform_do_upgrade() {
 			fi
 		else
 			if [ "$boot_part" -eq "1" ]; then
-				fw_setenv boot_part 2
+				echo "boot_part 2" >> $setenv_script
 				CI_KERNPART="alt_kernel"
 				CI_UBIPART="alt_rootfs"
 			else
-				fw_setenv boot_part 1
+				echo "boot_part 1" >> $setenv_script
 			fi
 		fi
-		fw_setenv boot_part_ready 3
-		fw_setenv auto_recovery yes
+
+		boot_part_ready="$(fw_printenv -n boot_part_ready)"
+		if [ "$boot_part_ready" -ne "3" ]; then
+			echo "boot_part_ready 3" >> $setenv_script
+		fi
+
+		auto_recovery="$(fw_printenv -n auto_recovery)"
+		if [ "$auto_recovery" != "yes" ]; then
+			echo "auto_recovery yes" >> $setenv_script
+		fi
+
+		if [ -n "$setenv_script" ]; then
+			fw_setenv -s $setenv_script || {
+				echo "failed to update U-Boot environment"
+				return 1
+			}
+		fi
 		nand_do_upgrade "$1"
 		;;
 	prpl,haze|\
