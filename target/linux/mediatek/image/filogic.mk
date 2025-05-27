81f3f4ec4b246f0cc2c2f8338371c6071abdf066
Paweł Owoc <frut3k7@gmail.com>
2025-05-28 00:46:46 +0200

    remove ubinize_opts
    Signed-off-by: Paweł Owoc <frut3k7@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -3160,7 +3160,6 @@ define Device/zyxel_ex5601-t0-stock
   DEVICE_DTS := mt7986a-zyxel-ex5601-t0-stock
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware kmod-usb3
-  UBINIZE_OPTS := -E 5
   BLOCKSIZE := 256k
   PAGESIZE := 4096
   IMAGE_SIZE := 65536k
@@ -3188,7 +3187,6 @@ define Device/zyxel_ex5601-t0-ubootmod
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware kmod-usb3
   KERNEL_INITRAMFS_SUFFIX := -recovery.itb
   IMAGES := sysupgrade.itb
-  UBINIZE_OPTS := -E 5
   BLOCKSIZE := 256k
   PAGESIZE := 4096
   KERNEL_IN_UBI := 1
